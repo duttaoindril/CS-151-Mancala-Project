a7303de3bdf8e0425e8cd15bcf3dd09e488bd64e
nthanonk <nanthana.thanonklin@sjsu.edu>
2016-05-07 10:57:17 -0700

    Added files via upload

--- Controller.java
+++ Controller.java
@@ -3,6 +3,8 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
+
+
 public class Controller implements ChangeListener {
    
 	private DataModel dataModel;
@@ -10,6 +12,12 @@ public class Controller implements ChangeListener {
     private StartPanelGUI startPanel;
     private StyleSelectionPanel styleFrame;
     
+    /**
+     * Controller instructor
+     * 
+     * @param g
+     * @param d
+     */
     public Controller(MancalaGUI g, DataModel d) {
         gui = g;
         dataModel = d;
@@ -19,29 +27,38 @@ public class Controller implements ChangeListener {
         attachControllers();
         stateChanged(null);
     }
-
+    /**
+     * attach the controller to dataModel, all of the buttons
+     */
     public void attachControllers() {
         for(PitButton btn : gui.getPits())
+        	
+        	//add actionListener to each pit button
             btn.addActionListener(new ActionListener() {
             	@Override
             	public void actionPerformed(ActionEvent e) {
                     dataModel.replaceState(new State(dataModel));
-                    dataModel.clicked(btn.getIndex());
+                    dataModel.clicked(btn.getIndex());//update index after clicking on the selected pit
                     dataModel.update();
                 }
             });
+        //add actionListener to undoBtn
         gui.getButton("undo").addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
                 dataModel.popUndo();
             }
         });
+        
+        //add actionListener to endBtn
         gui.getButton("end").addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
                 dataModel.switchTurn();
             }
         });
+        
+        //add actionListener to quitBtn
         gui.getButton("quit").addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
@@ -49,6 +66,8 @@ public class Controller implements ChangeListener {
                 System.exit(0);
             }
         });
+        
+        //add actionListener to changeBoardBtn
         gui.getButton("changeBoardBtn").addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -61,19 +80,20 @@ public class Controller implements ChangeListener {
          * Tell it to call gui.changeBoard when clicked
          * and pass changeBoard a reference to styleFrame's
          * MancalaAlter object.
-         * I think it's called getBoardColor()
+         * 
          */
       
         styleFrame.getDoneBtn().addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				gui.changeBoard(StyleSelectionPanel.getBoardSelection());	
-				gui.startGame(startPanel.getStartBoardColor());
+				gui.changeBoard(styleFrame.getBoardSelection());	
+				
 			}
         	
         });
         
+      //add actionListener to playBtn
         startPanel.getPlayBtn().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -82,7 +102,10 @@ public class Controller implements ChangeListener {
 			}
 		});
     }
-
+    
+    /**
+     * Updates view with current game state when changes are made to model.
+     */
     public void stateChanged(ChangeEvent e) {
         gui.getMancalaA().setText("A: "+dataModel.getPlayerAScore());
         gui.getMancalaB().setText("B: "+dataModel.getPlayerBScore());
--- StyleSelectionPanel.java
+++ StyleSelectionPanel.java
@@ -1,67 +1,33 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-/**
- * StyleSelectionFrame is the pop up frame to allow any player to change the board theme after the game starts
- * or during the game
- */
 public class StyleSelectionPanel extends JPanel {
 	
-	public static JButton doneBtn;
-	private static BoardSelectionPanel boardSelectionPanel;
+	public JButton doneBtn;
+	private BoardSelectionPanel boardThemeChanger;
 
 	public StyleSelectionPanel(){
-		/*
-		 * Just have to add some stuff here and hook it up to the Controller
-		 * then we are good! Good luck. If it doesn't work let me know and I 
-		 * can do it in the morning. But I'm sure you got this now. No worries.
-		 */
-		
-		/*
-		 * Note: Removed "implements MancalaAlter" since we can use 
-		 * action listeners to change the game board color.
-		 * The idea is that we save a MancalaAlter based on which
-		 * button gets clicked, then we can send that object to
-		 * MancalaGUI (through the Controller class) when the
-		 * "Done" button is clicked.
-		 */
-		 
-		/*
-		 * Note: All of the ActionListeners for selecting colors are in the 
-		 * BoardSelectionPanel class. Shouldn't have to update 
-		 * anything there. I made it earlier because we need the 
-		 * exact same stuff for the start menu.
-		 */
-		
 		//Setting width and height to match MancalaGUI
 		int width = 550;
 		int height = 250;
 		setSize(width, height);
 		
-		// We need to set the layout manager for this to a BorderLayout
-		this.setLayout(new FlowLayout());
+		//set the layout manager for this to a BorderLayout
+		this.setLayout(new BorderLayout());
 		
-		//Make boardSelectionPanel a new BoardSelectionPanel() here
-		BoardSelectionPanel boardThemeChanger = new BoardSelectionPanel();
+		//create a new object for BoardSelectionPanel
+		boardThemeChanger = new BoardSelectionPanel();
 		
-		/*
-		 * I went ahead and did this part since it was a little 
-		 * tricky in the other panel.
-		 * Just have to add it to boardSelectionPanel below.
-		 */
 		//Creating Done button wrapper
 		JPanel doneBtnPanel = new JPanel();
 		doneBtnPanel.setPreferredSize(new Dimension(this.getWidth(), 140));
 		doneBtnPanel.setBackground(Color.DARK_GRAY);
+		
 		//Creating Done button
 		doneBtn = new JButton("Done");
 		doneBtn.setPreferredSize(new Dimension(80, 45));
@@ -69,10 +35,7 @@ public class StyleSelectionPanel extends JPanel {
 		doneBtn.setFocusable(false);
 		doneBtn.setBorder(new EmptyBorder(10, 10, 10, 10));
 		doneBtnPanel.add(doneBtn);
-		
-		//Add doneBtnPanel to boardSelectionPanel so it will show up
-	    this.add(doneBtnPanel);
-		//I ran into some layout issues when I didn't on the other one
+		boardThemeChanger.add(doneBtnPanel);
 		
 		//Add boardSelectionPanel to this and set this to visible
 		this.add(boardThemeChanger);
@@ -80,13 +43,12 @@ public class StyleSelectionPanel extends JPanel {
 	}
 	
 	//getter method for returning a MancalaAlter object like boardSelectionPanel.getBoardSelection()
-	public static MancalaAlter getBoardSelection(){
-		return boardSelectionPanel.getBoardSelection();
+	public MancalaAlter getBoardSelection(){
+		return boardThemeChanger.getBoardSelection();
 	}
 	
 	//getter for returning the doneBtn, so Controller can use it
 	public JButton getDoneBtn(){
 		return doneBtn;		
 	}
-
 }
